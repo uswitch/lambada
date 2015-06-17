d2bf3fcfc7f7562f3514f278c3a69a3d68929e2a
Ragnar Dahlén <r.dahlen@gmail.com>
2015-06-17 20:17:34 +0100

    Update project with license

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lambada "0.1.0-SNAPSHOT"
+(defproject uswitch/lambada "0.1.0-SNAPSHOT"
+  :description "The messy parts for running clojure on AWS Lambda."
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-RC1"]
-                 [org.clojure/data.json "0.2.6"]
-                 [com.amazonaws/aws-lambda-java-core "1.0.0"]
-                 [com.amazonaws/aws-lambda-java-events "1.0.0"]]
-  :java-source-paths ["src-java"]
-  :uberjar-name "lambada.jar")
+                 [com.amazonaws/aws-lambda-java-core "1.0.0"]]
+  :java-source-paths ["src-java"])
