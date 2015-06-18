585e693c0d093ea2451073f92597884e344ac308
Ragnar Dahlén <r.dahlen@gmail.com>
2015-06-18 06:57:26 +0100

    Make example slightly more realistic.

--- example/project.clj
+++ example/project.clj
@@ -1,6 +1,7 @@
 (defproject my-lambda-project "0.1.0-SNAPSHOT"
   :description "Example lambada project."
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.6"]
                  [uswitch/lambada "0.1.0"]]
   :profiles {:uberjar {:aot :all}}
   :uberjar-name "my-lambda-project.jar")
--- example/src/example/lambda.clj
+++ example/src/example/lambda.clj
@@ -1,6 +1,15 @@
 (ns example.lambda
-  (:require [uswitch.lambada.core :refer [deflambdafn]]))
+  (:require [uswitch.lambada.core :refer [deflambdafn]]
+            [clojure.data.json :as json]
+            [clojure.java.io :as io]))
+
+(defn handle-event
+  [event]
+  (println "Got the following event: " (pr-str event))
+  {:status "ok"})
 
 (deflambdafn example.lambda.MyLambdaFn
   [in out ctx]
-  (println "OMG I'm running in the cloud!!!111oneone"))
+  (let [event (json/read (io/reader in))
+        res (handle-event event)]
+    (json/write res (io/writer out))))
