5b10ea98327a2b385818ff3b167a94c037980abe
Maris Orbidans <maris.orbidans@agentsmutual.co.uk>
2015-07-16 16:12:41 +0100

    another example

--- example/src/example/lambda.clj
+++ example/src/example/lambda.clj
@@ -14,3 +14,22 @@
         res (handle-event event)]
     (with-open [writer (io/writer out)]
       (json/write res writer))))
+
+;; fibonacci number example
+
+(defn fib-seq
+  ([] (fib-seq 0 1))
+  ([a b] (lazy-seq (cons a (fib-seq b (+' a b))))))
+
+(defn fibonacci-number-response
+  [event]
+  (let [index (get event "index" 0)]
+    {:index index
+     :fibonacci-number (nth (fib-seq) index)}))
+
+(deflambdafn example.lambda.FibonacciFn
+  [in out ctx]
+  (let [event (json/read (io/reader in))
+        res (fibonacci-number-response event)]
+    (with-open [writer (io/writer out)]
+      (json/write res writer))))
