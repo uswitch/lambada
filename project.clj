8548dd094bedf6fcad56debff3004c51db0c9bfa
Ragnar Dahlén <r.dahlen@gmail.com>
2016-06-13 13:53:53 +0100

    Bump deps

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "The messy parts for running clojure on AWS Lambda."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
-                 [com.amazonaws/aws-lambda-java-core "1.0.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.amazonaws/aws-lambda-java-core "1.1.0"]]
   :java-source-paths ["src-java"])
