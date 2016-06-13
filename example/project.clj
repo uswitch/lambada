8ff6fce52993faa53b353f0a975c0aef65f66611
Ragnar Dahlén <r.dahlen@gmail.com>
2016-06-13 15:20:33 +0100

    Bump deps in example project.

--- example/project.clj
+++ example/project.clj
@@ -1,7 +1,7 @@
 (defproject my-lambda-project "0.1.0-SNAPSHOT"
   :description "Example lambada project."
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [uswitch/lambada "0.1.0"]]
+                 [uswitch/lambada "0.1.2"]]
   :profiles {:uberjar {:aot :all}}
   :uberjar-name "my-lambda-project.jar")
